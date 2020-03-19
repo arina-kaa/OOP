a44fec375b96c6e8afbfdcae1e4c00a9c330bca2
arina-kaa <Arina.kaa@inbox.ru>
2020-03-19 23:49:19 +0300

    Добавила тесты для программы с вектором

--- lab_02_01_01/lab_02_01_01.cpp
+++ lab_02_01_01/lab_02_01_01.cpp
@@ -1,28 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <iterator>
-#include <numeric>
-#include <algorithm>
-
-void ProcessVector(std::vector<double>& numbers)
-{
-	size_t numberOfPositives = 0;
-	auto addIfPositive = [&numberOfPositives](double acc, double current) {
-		if (current > 0.0)
-		{
-			++numberOfPositives;
-			return acc + current;
-		}
-		return acc;
-	};
-
-	auto sumOfPositives = std::accumulate(numbers.begin(), numbers.end(), 0.0, addIfPositive);
-	double avgOfPositives = (numberOfPositives > 0) ? sumOfPositives / numberOfPositives : 0.0;
-
-	std::transform(numbers.begin(), numbers.end(), numbers.begin(), [avgOfPositives](double current) {
-		return current + avgOfPositives;
-	});
-}
+#include "ProcessVector.h"
 
 int main(int argc, char* argv[])
 {
